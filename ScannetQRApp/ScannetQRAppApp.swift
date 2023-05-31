49724ae1847a605e8aa223a8d85108db4a6a57b5
Kristel <idmaksimovak@gmail.com>
2023-05-31 13:27:56 +0300

    Initial Commit

---
+++ ScannetQRApp/ScannetQRAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScannetQRAppApp.swift
+//  ScannetQRApp
+//
+//  Created by Kristi on 31.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct ScannetQRAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
